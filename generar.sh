efe078a21829023843d42e04cbc5310d66f302b3
b4zz4 <bazza@riseup.net>
2013-06-27 16:35:29 -0300

    mejoras en el pagina

--- generar.sh
+++ generar.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cat README.md | pandoc -f markdown -c css/markdown.css -t html > index.html
+cat README.md | pandoc -f markdown \
+-c css/markdown.css \
+--table-of-contents \
+-t html5 \
+> index.html
